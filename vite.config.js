528dd5e08efcabc8e7de8369a34d98f0898db735
Oskar Wiederhold <oskar.wiederhold@adnovum.ch>
2026-08-20 16:50:01 +0200

    Test2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DEIN-REPO-NAME/',
+  base: '/publictest2/',
+  define: {
+    'process.env': {},
+  },
 })
